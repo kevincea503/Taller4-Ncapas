64bb3ddcb30cf566a3d5a0bf42fd7fb0e9452c2a
kevincea503 <00203919@uca.edu.sv>
2022-06-18 18:17:30 -0600

    add springRoles project

---
+++ springsecurityroles/src/main/java/com/uca/spring/ProductManagerApplication.java
@@ -0,0 +1,24 @@
+package com.uca.spring;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProductManagerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+	
+	  @Bean
+	  public ServletRegistrationBean h2servletRegistration() {
+	      ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+	      registration.addUrlMappings("/h2-console/*");
+	      return registration;
+	  }
+
+}
---
+++ springsecurityroles/src/main/java/com/uca/spring/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.uca.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.uca.spring.dto.MyUserDetails;
+import com.uca.spring.model.User;
+import com.uca.spring.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) 
+			throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		
+		return new MyUserDetails(user);
+	}
+
+}
