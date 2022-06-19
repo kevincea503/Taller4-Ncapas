1fd47ceb9370f5d99a9748477f767a8d32b8c50f
kevincea503 <00203919@uca.edu.sv>
2022-06-19 17:27:27 -0600

    finish

--- springsecurity/src/main/java/com/uca/spring/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uca.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uca.spring.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
